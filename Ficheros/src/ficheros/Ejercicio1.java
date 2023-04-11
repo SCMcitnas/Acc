6300830c53101a84de3461b13dbcdcb83caab20d
master <citnas5@gmail.com>
2023-04-11 13:56:50 +0200

    commit acc

--- Ficheros/src/ficheros/Ejercicio1.java
+++ Ficheros/src/ficheros/Ejercicio1.java
@@ -4,25 +4,42 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Scanner;
 
 public class Ejercicio1 {
 	public void divisionCart(int n, File fichero) throws IOException {
 		FileReader lector = new FileReader(fichero);
-		FileWriter escritor;
-		try{
+		
 			int i;
 			int archivos=0;
-			while((i=lector.read())!=-1) {
+			char a[] = new char[n];
+			while((i=lector.read(a))!=-1) {
 				archivos++;
 				File ficheroDiv= new File("C:\\Users\\Santiago\\Desktop\\texto"+archivos+".txt");
-				escritor= new FileWriter(ficheroDiv);
-				for(int j=0; j< n; j++) {
-					escritor.write((char)i);
+				ficheroDiv.createNewFile();
+				try(FileWriter escritor= new FileWriter(ficheroDiv)){
+					escritor.write(new String(a,0,i));
 				}
 			}
-		}finally {
-			if(lector!=null) {
-				lector.close();
+	}
+	
+	public void divisionLineas(int l,File fichero) throws IOException{
+		try(Scanner sc= new Scanner(fichero)){
+			
+			int archivos=0;
+			String mesg;
+			
+			while(sc.hasNext()) {
+				archivos++;
+				File ficheroDiv= new File("C:\\Users\\Santiago\\Desktop\\texto"+archivos+".txt");
+				
+				for(int i=0; i<l; i++) {
+					mesg=sc.nextLine();
+					try(PrintWriter pw = new PrintWriter(new FileWriter(ficheroDiv,true))){
+						pw.print(mesg+"\n");
+					}
+				}
 			}
 		}
 	}
@@ -31,6 +48,7 @@ public class Ejercicio1 {
 
         Ejercicio1 ejercicio1= new Ejercicio1();
         File fichero = new File("C:\\Users\\Santiago\\Desktop\\texto.txt");
-        ejercicio1.divisionCart(0,fichero);
+        //ejercicio1.divisionCart(3,fichero);
+        ejercicio1.divisionLineas(1,fichero);
     }
 }
